ef27341ad15079955952064fb705ca885eb39215
Pat O'Brien <98478494+Zooki0@users.noreply.github.com>
2024-08-02 01:03:35 +0100

    tried adding more movement code but not really good tbh lmao

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,21 +1,20 @@
 extends CharacterBody2D
 
 const move_speed := 80
-const jump_speed := 20
+const jump_speed := 9999999
 
 func jump(delta):
-	if is_on_floor():
-		velocity += Vector2.UP * jump_speed * delta
+	velocity += Vector2.UP * jump_speed * delta
 
 func move(delta):
 	if Input.is_action_pressed("left"):
 		velocity += Vector2.LEFT * move_speed * delta
 	elif Input.is_action_pressed("right"):
 		velocity += Vector2.RIGHT * move_speed * delta
-	elif Input.is_action_just_released("jump"):
+	elif Input.is_action_just_pressed("jump"):
 		jump(delta)	
 	else:
-		velocity = Vector2.ZERO
+		velocity = Vector2.DOWN * 5000 * delta
 	
 	move_and_slide()
 
